ee10ce782c82703041c588b3c7a409ff8ce444f9
unknown <pnltoen@gmail.com>
2024-01-01 03:52:24 +0900

    using metadata with Pixyz Plugin

--- Assets/usingMetadata.cs
+++ Assets/usingMetadata.cs
@@ -8,16 +8,25 @@ public class usingMetadata : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Metadata metadata = gameObject.GetComponent<Metadata>();
-
+        //Metadata metadata = gameObject.GetComponent<Metadata>();
+        ToAllChilds(transform);
     }
 
     void ToAllChilds(Transform parent)
     {
         for (int i = 0; i < parent.childCount; i++)
         {
-            GameObject child = parent.GetChild(i).gameObject;
-            Debug.Log(child);
+            Metadata metadata = parent.GetChild(i).gameObject.GetComponent<Metadata>();
+            if (metadata != null)
+            {
+                metadata.addOrSetProperty("Author", "pnltoen");
+            }
+            /*string name = metadata.getProperty("PartNumber");
+            if (name != null)
+            {
+                Debug.Log(name);
+            }*/
+            //GameObject child = parent.GetChild(i).gameObject;
             ToAllChilds(parent.GetChild(i));
         }
     }
